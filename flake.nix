eb333061199d250b4521b328b356e898c8f27d2b
isushilpuri <isushilpuri@gmail.com>
2026-02-24 16:05:25 +0530

    Add nvf config

--- flake.nix
+++ flake.nix
@@ -6,19 +6,32 @@
        url = "github:nix-community/home-manager";
        inputs.nixpkgs.follows = "nixpkgs";
     };
+    nvf.url = "github:notashelf/nvf";
     # # Dotfiles flake
     # nix-dots.url = "path:/home/v0idshil/nix-dots"; # Change path if needed
   };
-  outputs = { self, nixpkgs, home-manager, ... }: 
+  outputs = { self, nixpkgs, home-manager, nvf, ... }: 
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages."$(system)";
     in
     {
+      packages.${system}.default =
+	      (nvf.lib.neovimConfiguration {
+		inherit pkgs;
+		modules = [
+		  ./nvf/nvf_conf.nix
+		];
+	      })
+	      .neovim;
+
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [ 
             ./configuration.nix
+            ({pkgs, ...}: {
+              environment.systemPackages = [self.packages.${pkgs.stdenv.system}.neovim];
+            })
 	    home-manager.nixosModules.home-manager
 	    {
 		home-manager = {
