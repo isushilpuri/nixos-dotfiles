b6046ffb84c0406343fe12833467ab8e456e0d06
isushilpuri <isushilpuri@gmail.com>
2026-04-15 13:54:27 +0530

    System Gen 42 2026-04-15 13:54:24

--- home.nix
+++ home.nix
@@ -60,6 +60,7 @@ in
     kdePackages.dolphin
     swaylock-effects
     sway
+    python314Packages.weasyprint
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
