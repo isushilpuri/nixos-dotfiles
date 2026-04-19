ab0b963684962b894c18ffe30c30a07cc11816e6
isushilpuri <isushilpuri@gmail.com>
2026-04-20 00:58:28 +0530

    System Gen 44 2026-04-20 00:58:25

--- home.nix
+++ home.nix
@@ -57,13 +57,14 @@ in
     gimp
     distrobox
     nautilus
-    kdePackages.dolphin
     swaylock-effects
     sway
     python314Packages.weasyprint
     onlyoffice-desktopeditors
-    kdePackages.gwenview
     qimgv
+    gthumb
+    corefonts
+    vista-fonts
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
