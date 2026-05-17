26dc92afadfdeb6c41a50ea766c66ada304ffe25
isushilpuri <isushilpuri@gmail.com>
2026-05-17 15:33:24 +0530

    System Gen 48 2026-05-17 15:33:21

--- home.nix
+++ home.nix
@@ -66,6 +66,7 @@ in
     corefonts
     vista-fonts
     guvcview
+    google-chrome
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
