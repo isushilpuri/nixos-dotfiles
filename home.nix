9ee73c90b25b352b503e6a79c8264950d64c3ebb
isushilpuri <isushilpuri@gmail.com>
2026-06-30 20:03:26 +0530

    System Gen 52 2026-06-30 20:03:23

--- home.nix
+++ home.nix
@@ -70,6 +70,7 @@ in
     google-chrome
     kdePackages.kdenlive
     discord
+    localsend
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
