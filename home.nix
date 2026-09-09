b06a51a648fae7417f09a1a2df5a252feb3781d9
isushilpuri <isushilpuri@gmail.com>
2026-09-09 22:31:36 +0530

    System Gen 64 2026-09-09 22:31:33

--- home.nix
+++ home.nix
@@ -74,6 +74,7 @@ in
     kdePackages.kdenlive
     discord
     localsend
+    motrix-next
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
