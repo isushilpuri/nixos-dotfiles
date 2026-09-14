7111fb54189daac83fbc0826a4df2d359b70a0b2
isushilpuri <isushilpuri@gmail.com>
2026-09-14 16:21:01 +0530

    System Gen 67 2026-09-14 16:20:58

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,22 @@
       "rd.udev.log_level=3"
       "rd.systemd.show_status=auto"
     ];
+    plymouth = {
+      enable = true;
+      theme = "v0idshil";
+
+      themePackages = [
+        (pkgs.runCommand "plymouth-v0idshil-theme" {} ''
+          mkdir -p $out/share/plymouth/themes/v0idshil
+
+          cp ${./plymouth-v0idshil/v0idshil.plymouth} \
+              $out/share/plymouth/themes/v0idshil/v0idshil.plymouth
+
+          cp ${./plymouth-v0idshil/v0idshil.script} \
+              $out/share/plymouth/themes/v0idshil/v0idshil.script
+        '')
+      ];
+    };
   };
 
   networking.hostName = "nixos"; # Define your hostname.
